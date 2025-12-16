0caca58ca20735ab12140880854e0e7f7be77a29
muhhlmy <muhhlmy@gmail.com>
2025-12-16 14:37:25 +0700

    refactor: update nuxt.config.ts structure and formatting

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,11 +1,21 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+  runtimeConfig: {
+    secret: {},
+    public: {
+      //GoogleClient: process.env.GOOGLE_CLIENT_ID,
+      //BaseUrl: process.env.BASE_URL, // bisa dipakai di frontend
+    },
+  },
   compatibilityDate: "2024-11-01",
+  app: {
+    head: {
+      title: "Inkowapi",
+    },
+  },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
